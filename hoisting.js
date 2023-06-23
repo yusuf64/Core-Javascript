983ce3e40e5873f6ac6137228765f9e07f593371
Yusuf <ky69452@gmail.com>
2023-06-24 02:22:42 +0530

    Added a new summary of hoisting

--- hoisting.js
+++ hoisting.js
@@ -15,6 +15,15 @@ function getName() {
 
 var c = 786;
 
+// SHORT AND SWEET SUMMARY PLEASE READ THIS
+
+// 1. In JS, before the code is executed, the variables get initialized to undefined.
+// 2. Arrow functions enact as variables and get "undefined" during the memory creation phase while functions actually get run.
+// 3. Hoisting: Mechanism in JS where the variable declarations are moved to the top of the scope before execution. Therefore it is possible to call a function before initializing it.
+// 4. Whenever a JS program is run, a global execution block is created, which comprises of 2: Memory creation and Code execution.
+// 5. Variable declarations are scanned and are made undefined
+// 6. Function declarations are scanned and are made available
+
 // &&&&&&&&&& NOTE &&&&&&&&&&&&&&
 
 // As you can see here we are accessing teh variable name first by console.log and then we are declaring it here we can see the the value of c undefinded this is because instead of giving error all the varibale decalre are stored in memorey allocation context where the values of theat variable sets to undefined and when we execute that code the js will execute each code line by line so first it will gets the value undefinde of c variable adn for the function it gets stored as it is
