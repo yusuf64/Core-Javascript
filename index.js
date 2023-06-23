722292ee1b3e051ed0009a9453889885c82ba8ca
Yusuf <ky69452@gmail.com>
2023-06-24 00:56:49 +0530

    Hello this is me again

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ console.log("Hello node js");
 
 var a = 5;
 console.log(a);
+
+function square(num) {
+  var ans = num * num;
+  return ans;
+}
+
+var b = square(a);
+var c = square(5);
+console.log(b, c);
