e4f7c36ef50fb66fdf1c955154ac305d22cb696f
Yusuf <ky69452@gmail.com>
2023-06-24 01:11:14 +0530

    dd

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-console.log("Hello node js");
-
-var a = 5;
+var a;
 console.log(a);
-
-function square(num) {
-  var ans = num * num;
-  return ans;
-}
-
-var b = square(a);
-var c = square(5);
-console.log(b, c);
+a = 12;
+console.log(a);
