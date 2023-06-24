5258e50a143415eae171c23c7d97ce22cf6df384
Yusuf <ky69452@gmail.com>
2023-06-25 02:20:47 +0530

    added a new shortest js program

---
+++ shortest.js
@@ -0,0 +1,6 @@
+// Summary
+
+// 1. Shortest Program in JS: Empty file. Still, browsers make global EC and global space along with Window object.
+// 2. Global Space: Anything that is not in a function, is in the global space.
+// 3. Variables present in a global space can be accessed by a "window" object. (like window.a)
+// 4. In global space, (this === window) object.
