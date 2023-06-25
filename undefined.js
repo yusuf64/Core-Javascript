1248417749782655a9e4c79b12a4e39376b4e23e
Yusuf <ky69452@gmail.com>
2023-06-25 17:01:48 +0530

    Added some notes

--- undefined.js
+++ undefined.js
@@ -3,3 +3,7 @@ console.log(a);
 
 var a = 10;
 console.log(a);
+
+// As you can see when we are tring to console a before assigning a value wwe will get undefined this is becuase undefined is nothing but a placeholder which will assign to a variable if its initialed to any real value and when any value is initalies in runt ime to that variable it will act as a place holder and store that real value
+
+// This is because js is weekly or loosly typed lanugage it allocates the varibale at run time and in that variable we cna store any data types unlike other programming lanugaes where we we dfined the data type too whilw daelcaring it
